4aa2152579af5ec64c1a8a2983ce75953a643410
Sumit Verma <sumitverma@Sumits-MacBook-Air.local>
2022-03-16 14:48:03 +0000

    scp commit

---
+++ book/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from routes import book_blueprint
+from models import db, init_app
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5fPxNUWP2Srzded6fayMeA'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/book.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.register_blueprint(book_blueprint)
+init_app(app)
+
+migrate = Migrate(app, db)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
---
+++ frontend/api/__init__.py
@@ -0,0 +1,7 @@
+#USER_API_URL = 'http://user-service-c:5001'
+USER_API_URL = 'http://127.0.0.1:5001'
+BOOK_API_URL = 'http://127.0.0.1:5002'
+ORDER_API_URL = 'http://127.0.0.1:5003'
+#BOOK_API_URL = 'http://book-service-c:5002'
+#ORDER_API_URL = 'http://order-service-c:5003'
+
