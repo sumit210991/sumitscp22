66730492d35fbd74c434abb1f72410541345cfe2
Sumit Verma <sumitverma@Sumits-MacBook-Air.local>
2022-04-12 01:11:43 +0100

    first commit

--- book/app.py
+++ book/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5fPxNUWP2Srzded6fayMeA'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Advik%4009@localhost/elib'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sumit2109@database-1.cmmco8ooibwj.us-east-1.rds.amazonaws.com/book'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.register_blueprint(book_blueprint)
 init_app(app)
--- classroom/app.py
+++ classroom/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "pSaOCtBJ90oNkW9nigTXAw"
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Advik%4009@localhost/classroom'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:sumit2109@database-1.cmmco8ooibwj.us-east-1.rds.amazonaws.com/classroom'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.register_blueprint(classroom_blueprint)
